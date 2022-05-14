80aba134327bfb4496de851135683963a56ba88f
naim <naim2206@yahoo.com.mx>
2022-05-13 22:02:38 -0500

    el juego 2 ya jala  mas chilo

--- src/main/game2/GameStates2.java
+++ src/main/game2/GameStates2.java
@@ -6,6 +6,7 @@ import java.awt.RenderingHints;
 
 import game.Assets;
 import game.GameState;
+import physics.Bolder;
 import physics.Brick;
 import physics.Material;
 import java.awt.*;
@@ -16,24 +17,34 @@ public class GameStates2 extends GameState {
 
 	public GameStates2() {
 		colObjects.add(new Brick(0, heigth - 25, 60, Material.Stone, width, 25, this));
+		colObjects.add(new Brick(0, 25, 60, Material.Stone, width, 25, this));
 		for (int i = 0; i < width; i += 100) {
-			for (int j = 0; j < 300; j += 100) {
-				colObjects.add(new Brick(i, j, 1, Material.Wood, 80, 80, this));
+			for (int j = 100; j < 400; j += 100) {
+				colObjects.add(new Brick(i, j, -100, Material.Wood, 80, 80, this));
 			}
 		}
 		// colObjects.add(new Brick(0, 0, 1, Material.Wood, 80, 80, this));
 
-		this.player = new MyPlayer2(100, heigth - 50, 102, 0, 0, 0, 0, 25,
+		this.player = new MyPlayer2(100, heigth - 100, 102, 0, 0, 0, 0, 50,
 				Assets.Player, this);
 		colObjects.add(player);
+		colObjects.add(new Bolder(100, 400, 1, 0, 50, 0, 0, 50, this));
 
 	}
 
 	public void update() {
 
 		for (int i = 0; i < colObjects.size(); i++) {
+			if (colObjects.get(i) instanceof Bolder) {
+				if (colObjects.get(i).getX() > width) {
+					colObjects.get(i).setX(0);
+				}
+				if (colObjects.get(i).getX() < 0) {
+					colObjects.get(i).setX(width - 1);
+				}
+				// System.out.println(colObjects.get(i).getWeight());
+			}
 			colObjects.get(i).update();
-			// System.out.println(colObjects.get(i).getWeight());
 		}
 		// System.out.println();
 
